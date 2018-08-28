4829c9cb695e3672d0cdaa1d995204f49f126e13
Bigwan <BigWan@users.noreply.github.com>
2018-08-28 18:03:15 +0800

    新建项目

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 弹跳的方块
+/// </summary>
+public class Blocks : MonoBehaviour {
+
+
+    
+}
---
+++ Assets/Scripts/Role.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 跳一跳的小人
+/// </summary>
+public class Role : MonoBehaviour {
+
+    public int totalScore = 0;
+
+    /// <summary>
+    /// 跳跃速度(按压一秒跳跃的距离)
+    /// </summary>
+    public float jumpSpeed = 10f;
+
+
+    /// <summary>
+    /// 跳跃的时间
+    /// </summary>
+    public float jumpTime = 1f;
+
+
+    /// <summary>
+    /// 跳跃的曲线控制(抛物线)
+    /// </summary>
+    public AnimationCurve curve;
+
+
+    public Vector3 currentToward;
+
+    private bool isJumping;
+
+
+
+    public void StartJump(float pressTime = 1f) {
+
+        StartCoroutine(JumpCoroutine(pressTime));
+    }
+
+    public IEnumerator JumpCoroutine(float pressTime) {
+
+        Vector3 targetPos = transform.localPosition + pressTime * jumpSpeed * currentToward;
+
+
+
+        yield return null;
+    }
+
+    private void Update() {
+        
+    }
+
+}
